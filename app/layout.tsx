a9bd077c78cd27beb98e176813e17a3adc0e2c7a
Alfayed <alfayed.msa@etu.u-pec.fr>
2026-06-04 11:49:26 +0200

    Newsletter Data

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,23 +75,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr" className={roboto.variable}>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      </head>
       <body className="font-roboto bg-[#080010] text-text-main antialiased overflow-x-hidden">
         {children}
       </body>
       <Script
-  src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-  strategy="afterInteractive"
-/>
-<Script id="ga-init" strategy="afterInteractive">
-  {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
-      send_page_view: false
-    });
-  `}
-</Script>
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="ga-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+            send_page_view: false
+          });
+        `}
+      </Script>
     </html>
   );
 }
